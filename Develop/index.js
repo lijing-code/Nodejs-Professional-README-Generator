26635364c51967a7b925c05709e7e9e4c63bfcc3
Jing Li <lijing900913@gmail.com>
2021-10-27 14:46:35 -0400

    Update the details for each function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        message:'What is your project title?',
+        name:'projectTitle',
+    },
+    {
+        type:'input',
+        message:'What is your description of this project?',
+        name:'description',
+    },
+    {
+        type:'input',
+        message:'What are the steps required to install your project? \n Use ";" to seperate each steps, no space between each steps.',
+        name:'installation',
+    },
+    {
+        type:'input',
+        message:'Provide instructions and examples for use.',
+        name:'usage',
+    },
+    {
+        type:'input',
+        message:'Please enter the url for the screenshot for the project',
+        name:'usageScreenshot',
+    },
+    {
+        type:'input',
+        message:'List your collaborators, if any, with links to their GitHub profiles. \n Use ";" to seperate each person. no space between each steps.(name: githublink;)',
+        name:'creditsForCollaborators',
+    },
+    {
+        type:'input',
+        message:'If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.\n Use ";" to seperate each recources. no space between each steps.(Resource name: resource link;)',
+        name:'creditsForResources',
+    },
+    {
+        type:'input',
+        message:'Guideline for users to test this project: \n Use ";" to seperate each steps. no space between each steps.',
+        name:'test',
+    },
+    {
+        type:'checkbox',
+        message:'License:',
+        name:'license',
+        choices: ['MIT', 'GPLv3', 'Apache', 'ISC']
+    },
+    {
+        type:'input',
+        message:'If your project has a lot of features, list them here.\n Use ";" to seperate each features. no space between each steps.',
+        name:'features',
+    },
+    {
+        type:'input',
+        message:"If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.\n The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.",
+        name:'contributor',
+    },
+    {
+        type:'input',
+        message:'What is your Github username?',
+        name:'username',
+    },
+    {
+        type:'input',
+        message:'What is your email',
+        name:'email',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    // const fileName = `${data.projectTitle.split(' ').join('')}.md`;
+    fs.writeFile('README.md',`${generateMarkdown(data)}`, (err) =>
+      err ? console.error(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data)=>writeToFile(data));
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,54 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  const formatTheAnswer = (string) =>{
+    const list = string.split(';');
+    let bulletPoint = "";
+    list.forEach((item)=> {bulletPoint +=`- ${item}\n`});
+    return bulletPoint;
+  }
+  return `# ${data.projectTitle}
+  ---
+  ## Description
+  ${data.description}
+  ---
+  ## Table of Contents 
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  ---
+  ## Installation
+  ${formatTheAnswer(data.installation)}
+  ---
+  ## Usage
+  - ${data.usage}
+  - ${data.usageScreenshot}
+  ---
+  ## Credits
+  ---
+  ### People
+  ${formatTheAnswer(data.creditsForCollaborators)}
+  ---
+  ### Resource
+  ${formatTheAnswer(data.creditsForResources)}
+  ---
+  ## License
+  ![${data.license}](https://img.shields.io/static/v1?label=license&message=${data.license}&color=brightgreen&style=plastic&logo=appveyor)
+  ---
+  ## Features
+  ${formatTheAnswer(data.features)}
+  ---
+  ## How to Contribute
+  [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
+  ${formatTheAnswer(data.contributor)}
+  ---
+  ## Tests
+  ${formatTheAnswer(data.test)}
+  ---
+  ## Questions
+  - My GitHub is [${data.username}](https://github.com/${data.username}), please feel free to ask me if you have any question about my project.
+  - My email is ${data.email}.
 `;
 }
 
